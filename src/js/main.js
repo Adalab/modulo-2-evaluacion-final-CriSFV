3cdbeba8c1bd2c6261a83ca80d676dc44e199a0e
CrS-21 <cristina.f.valverde@gmail.com>
2021-09-09 17:51:44 +0200

    add placeholder when it's without image and change some classes

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,29 @@
 'use strict';
-let userSearch = document.querySelector('.js_userName');
+const userSearch = document.querySelector('.js_userText');
 const searchButton = document.querySelector('.js_searchButton');
-const filmList = document.querySelector('.filmList');
+const filmList = document.querySelector('.js_filmList');
 
-function eraserClick(ev) {
-  ev.preventDefault();
+function submitDefault(event) {
+  event.preventDefault();
 }
-userSearch.addEventListener('submit', eraserClick);
+userSearch.addEventListener('submit', submitDefault);
 
 function searchFilm() {
   const userSearchValue = userSearch.value;
-
   fetch(`//api.tvmaze.com/search/shows?q=${userSearchValue}`)
     .then((response) => response.json())
     .then((data) => {
       for (const eachdata of data) {
-        filmList.innerHTML += `<li><img src="${eachdata.show.image.medium}" alt="Caratula"><h3 class="film-title">${eachdata.show.name}</h3></li>`;
+        if (eachdata.show.image === null) {
+          filmList.innerHTML += `<li class="film"><img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" alt="Caratula"><h3 class="film-title">${eachdata.show.name}</h3></li>`;
+        } else {
+          filmList.innerHTML += `<li class="film"><img src="${eachdata.show.image.medium}" alt="Caratula"><h3 class="film__title">${eachdata.show.name}</h3></li>`;
+        }
       }
     });
 }
 
-function handleClickButton(ev) {
-  console.log(ev.currentTarget);
-  console.log(ev.target);
+function handleClickButton() {
   searchFilm();
 }
 
