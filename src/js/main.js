10f21d771ecb0ea516f1a44135261b70546471e4
CrS-21 <cristina.f.valverde@gmail.com>
2021-09-10 20:20:05 +0200

    add isInFavorite for paint films selected

--- src/js/main.js
+++ src/js/main.js
@@ -12,8 +12,8 @@ function submitDefault(event) {
 form.addEventListener('submit', submitDefault);
 
 function paintFavorites(ev) {
-  const filmSelected = ev.target.parentElement;
-  filmSelected.classList.toggle('film__selected');
+  // const filmSelected = ev.target.parentElement;
+  // filmSelected.classList.toggle('film__selected');
   favoriteList.innerHTML = '';
   for (const fav of favorites) {
     if (fav.show.image === null) {
@@ -28,16 +28,17 @@ function handleFilmSelected(ev) {
   const filmSelectedId = parseInt(ev.target.parentElement.id);
   const filmClicked = dataFilms.find((film) => film.show.id === filmSelectedId);
   console.log(favorites);
-  const isInFavorites = favorites.findIndex((fav) => {
+  const numberInFavorites = favorites.findIndex((fav) => {
     return fav.show.id === filmSelectedId;
   });
 
-  if (isInFavorites === -1) {
+  if (numberInFavorites === -1) {
     favorites.push(filmClicked);
   } else {
-    favorites.splice(isInFavorites, 1);
+    favorites.splice(numberInFavorites, 1);
   }
   paintFavorites(ev);
+  paintFilms();
 }
 
 function selectedFilmListener() {
@@ -46,22 +47,42 @@ function selectedFilmListener() {
     eachfilm.addEventListener('click', handleFilmSelected);
   }
 }
-
-function getApi() {
+function isInFavorite(eachdata) {
+  const favorite = favorites.find((fav) => {
+    return fav.show.id === eachdata.show.id;
+  });
+  if (favorite === undefined) {
+    return false;
+  } else {
+    return true;
+  }
+}
+function paintFilms() {
   filmList.innerHTML = '';
+  let selectedClass = '';
+  for (const eachdata of dataFilms) {
+    const isFav = isInFavorite(eachdata);
+    console.log(isFav);
+    if (isFav) {
+      selectedClass = 'film__selected';
+    } else {
+      selectedClass = '';
+    }
+    if (eachdata.show.image === null) {
+      filmList.innerHTML += `<li class="film js_film ${selectedClass}" id="${eachdata.show.id}"><img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" alt="Caratula"><h3 class="film__title">${eachdata.show.name}</h3></li>`;
+    } else {
+      filmList.innerHTML += `<li class="film js_film ${selectedClass}" id="${eachdata.show.id}"><img src="${eachdata.show.image.medium}" alt="Caratula"><h3 class="film__title">${eachdata.show.name}</h3></li>`;
+    }
+    selectedFilmListener();
+  }
+}
+function getApi() {
   const userSearchValue = userSearch.value;
   fetch(`//api.tvmaze.com/search/shows?q=${userSearchValue}`)
     .then((response) => response.json())
     .then((data) => {
       dataFilms = data;
-      for (const eachdata of data) {
-        if (eachdata.show.image === null) {
-          filmList.innerHTML += `<li class="film js_film" id="${eachdata.show.id}"><img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" alt="Caratula"><h3 class="film__title">${eachdata.show.name}</h3></li>`;
-        } else {
-          filmList.innerHTML += `<li class="film js_film" id="${eachdata.show.id}"><img src="${eachdata.show.image.medium}" alt="Caratula"><h3 class="film__title">${eachdata.show.name}</h3></li>`;
-        }
-        selectedFilmListener();
-      }
+      paintFilms();
     });
 }
 
