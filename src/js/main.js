be7842d677dd9c539462d6a5fdca65f635f24f8d
CrS-21 <cristina.f.valverde@gmail.com>
2021-09-10 13:51:57 +0200

    add funtion paint favorites

--- src/js/main.js
+++ src/js/main.js
@@ -11,21 +11,33 @@ function submitDefault(event) {
 }
 form.addEventListener('submit', submitDefault);
 
-// function paintFavorites(){
-//   favoriteList.innerHTML+=`<li class="film js_film" id="${eachdata.show.id}"><img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" alt="Caratula"><h3 class="film__title">${eachdata.show.name}</h3></li>`
-// }
+function paintFavorites(ev) {
+  const filmSelected = ev.target.parentElement;
+  filmSelected.classList.toggle('film__selected');
+  favoriteList.innerHTML = '';
+  for (const fav of favorites) {
+    if (fav.show.image === null) {
+      favoriteList.innerHTML += `<li class="film js_film" id="${fav.show.id}"><img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" alt="Caratula"><h3 class="film__title">${fav.show.name}</h3></li>`;
+    } else {
+      favoriteList.innerHTML += `<li class="film js_film" id="${fav.show.id}"><img src="${fav.show.image.medium}" alt="Caratula"><h3 class="film__title">${fav.show.name}</h3></li>`;
+    }
+  }
+}
 
 function handleFilmSelected(ev) {
-  const filmSelected = ev.target.parentElement;
   const filmSelectedId = parseInt(ev.target.parentElement.id);
+  //si quiero que me lo añada:
+  const filmClicked = dataFilms.find((film) => film.show.id === filmSelectedId); //ultima pinchada la busca en el array de dataFilm
 
-  const filmClicked = dataFilms.find((film) => film.show.id === filmSelectedId);
-  favorites.push(filmClicked);
+  favorites.push(filmClicked); // incluye ultima pinchada en favoritos
 
-  console.log(favorites); //me añade al array
+  // const filmClickedFavorite = favorites.findIndex(
+  //   (fav) => fav.show.id === filmSelectedId
+  // );
+  // console.log(filmClickedFavorite);
 
-  filmSelected.classList.toggle('film__selected');
-
+  paintFavorites(ev);
+}
 
 function selectedFilmListener() {
   const filmSearched = document.querySelectorAll('.js_film');
